128e153dd4c6fe0a1dea2a9663325832e5bc45dd
rizal <thoyyiburrizal@gmail.com>
2025-03-11 10:42:48 +0700

    commit pertama

---
+++ dart_project/bin/TUGAS 1/2.dart
@@ -0,0 +1,9 @@
+import 'dart:io';
+
+void main() {
+  stdout.write("Masukkan nama Anda: ");
+  String? nama = stdin.readLineSync();
+
+  print('Hello, saya `$nama`');
+  print("Hello, saya \"$nama\"");
+}
